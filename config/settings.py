b2a90d762b53e2769574abb38b1685a153b5aebf
pjmendoz <pearl.j.mendoza@gmail.com>
2019-10-21 15:10:26 -0400

    grabbing my heading and have bullet points

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.notes'
+    'notes',
 ]
 
 MIDDLEWARE = [
--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from notes.data import NOTES
 
 # Create your views here.
+def notes_view(request): 
+    return render(request, "notes/notes_view.html", {
+        "notes": NOTES,
+    })
