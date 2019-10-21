316acb17e341220b7490b31b2701fc8034cbf8d5
pjmendoz <pearl.j.mendoza@gmail.com>
2019-10-21 16:49:49 -0400

    changing notes_view to notes_list

--- notes/views.py
+++ notes/views.py
@@ -3,7 +3,7 @@ from notes.data import NOTES
 
 # Create your views here.
 def notes_view(request): 
-    return render(request, "notes/notes_view.html", {
+    return render(request, "notes/notes_list.html", {
         "notes": NOTES,
     })
 
