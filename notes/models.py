4e4f9ca09c019922b40bd69eee8e4057fbcf04bf
pjmendoz <pearl.j.mendoza@gmail.com>
2019-10-22 14:04:00 -0400

    creating model and admin

--- notes/models.py
+++ notes/models.py
@@ -4,7 +4,12 @@ from django.db import models
 
 class Note(models.Model): 
     title = models.CharField(max_length=100)
-    description = models.TextField(help_text="Any instructions people need to use this note app.", blank=True, null=True)
+    description = models.TextField(
+        help_text="Any instructions people need to use this note app.", 
+        blank=True, 
+        null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)    
     
     def __str(self):
         return self.title
